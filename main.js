5f2f7604fdc09698b3f092b64c200d8af900a5eb
Dave <david.w@fundamentalvr.com>
2020-04-06 10:39:45 +0100

    Fix date value

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function configDstPath() {
   if (month < 10) {
     month = '0' + month;
   }
-  let day = date.getDay() + 1;
+  let day = date.getDate();
   if (day < 10) {
     day = '0' + day;
   }
